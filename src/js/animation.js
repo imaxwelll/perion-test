c7bd1869b89d8d5b123fbbddcf528fa8cde5a07d
Max Bilashenko <imaxvelll@gmail.com>
2020-02-26 21:52:13 +0200

    delete non actual links from index file

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,48 +1,46 @@
-import AOS from 'aos';
+import AOS from "aos";
 
 // ..
 AOS.init({
-    offset: 900
-   
-  })
+  offset: 900
+});
 
-
-  let debounce_timer;
-window.onscroll = function(){
-        if(debounce_timer) {
-                window.clearTimeout(debounce_timer);
-        }
-        debounce_timer = window.setTimeout(function() {
-          if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-            let left = document.querySelectorAll('.disappearing');
-            for (let i = 0; i < left.length; i++) {
-              left[i].classList.remove("aos-animate")
-            }
-            // left.classList.remove("aos-animate")
-                console.log('Fire');
-          }
-        }, 6000);
+let debounce_timer;
+window.onscroll = function() {
+  if (debounce_timer) {
+    window.clearTimeout(debounce_timer);
+  }
+  debounce_timer = window.setTimeout(function() {
+    if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+      let left = document.querySelectorAll(".disappearing");
+      for (let i = 0; i < left.length; i++) {
+        left[i].classList.remove("aos-animate");
+      }
+      // left.classList.remove("aos-animate")
+      console.log("Fire");
+    }
+  }, 6000);
 };
 
-document.querySelector('#toggle2').onclick = closeBlock;
+document.querySelector("#toggle2").onclick = closeBlock;
 
-function closeBlock () {
-  var block = document.querySelector('.teaser-wrapper');
+function closeBlock() {
+  var block = document.querySelector(".teaser-wrapper");
   if (!block) {
     return;
   }
-  return block.hidden= !block.hidden;
+  return (block.hidden = !block.hidden);
 }
 
-var block = document.querySelector('.toggleAction');
+var block = document.querySelector(".toggleAction");
 
-Array.from(document.querySelectorAll('#toggle'), function(el){
+Array.from(document.querySelectorAll("#toggle"), function(el) {
   el.onclick = function() {
-    if (block.style.display === 'none') {
-      block.style.display = 'flex';
+    if (block.style.display === "none") {
+      block.style.display = "flex";
     } else {
-      block.style.display = 'none';
-    };
+      block.style.display = "none";
+    }
     return;
-  }
-})
+  };
+});
