4284dae6b65664711d97f982a91eb44dc19cee52
Max Bilashenko <imaxvelll@gmail.com>
2020-02-26 16:37:43 +0200

    adding perion-test to repository

---
+++ src/js/animation.js
@@ -0,0 +1,48 @@
+import AOS from 'aos';
+
+// ..
+AOS.init({
+    offset: 900
+   
+  })
+
+
+  let debounce_timer;
+window.onscroll = function(){
+        if(debounce_timer) {
+                window.clearTimeout(debounce_timer);
+        }
+        debounce_timer = window.setTimeout(function() {
+          if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+            let left = document.querySelectorAll('.disappearing');
+            for (let i = 0; i < left.length; i++) {
+              left[i].classList.remove("aos-animate")
+            }
+            // left.classList.remove("aos-animate")
+                console.log('Fire');
+          }
+        }, 6000);
+};
+
+document.querySelector('#toggle2').onclick = closeBlock;
+
+function closeBlock () {
+  var block = document.querySelector('.teaser-wrapper');
+  if (!block) {
+    return;
+  }
+  return block.hidden= !block.hidden;
+}
+
+var block = document.querySelector('.toggleAction');
+
+Array.from(document.querySelectorAll('#toggle'), function(el){
+  el.onclick = function() {
+    if (block.style.display === 'none') {
+      block.style.display = 'flex';
+    } else {
+      block.style.display = 'none';
+    };
+    return;
+  }
+})
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import '../style/normalize.css';
+import '../style/base.scss';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+console.log("Hello, it`s me");
+import './animation';
